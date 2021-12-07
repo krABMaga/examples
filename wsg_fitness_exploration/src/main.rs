7938ba29048b23c162e4cd11753ee19c8848b4cb
ddevin96 <danieledevinco1996@gmail.com>
2021-12-07 10:08:52 +0100

    fix genetic algorithm
    Co-authored-by: Luca Postiglione <zioposty@users.noreply.github.com>
    Co-authored-by: Giuseppe <gdambrosio@unisa.it>

--- wsg_fitness_exploration/src/main.rs
+++ wsg_fitness_exploration/src/main.rs
@@ -20,14 +20,16 @@ pub const MUTATION_RATE: f64 = 0.05;
 pub const INITIAL_NUM_WOLVES: u32 = (100. * 0.4) as u32;
 pub const INITIAL_NUM_SHEEPS: u32 = (100. * 0.6) as u32;
 
-pub const WIDTH: i32 = 20;
-pub const HEIGHT: i32 = 20;
-pub const STEP: u64 = 10;
+pub const WIDTH: i32 = 50;
+pub const HEIGHT: i32 = 50;
+pub const STEP: u64 = 100;
 
 use {
     rust_ab::engine::{schedule::Schedule, state::State},
     rust_ab::*,
+    rust_ab::rand::*,
 };
+use rand::seq::SliceRandom;
 
 fn main() {
     ga!(
@@ -37,7 +39,7 @@ fn main() {
         mutation,
         crossover,
         WsgState,
-        1000.,
+        0.98,
         30,
         STEP
     );
@@ -54,7 +56,7 @@ fn init_population() -> Vec<WsgState> {
         let gain_energy_wolf = rng.gen_range(1.0..=50.0);
         let sheep_repr = rng.gen_range(0.01..=0.2);
         let wolf_repr = rng.gen_range(0.0..=0.2);
-        let full_grown = rng.gen_range(0..100);
+        let full_grown = rng.gen_range(10..40);
 
         let state = WsgState::new(
             gain_energy_sheep,
@@ -71,72 +73,138 @@ fn init_population() -> Vec<WsgState> {
 }
 
 fn selection(population: &mut Vec<WsgState>) {
-    // sort population for fitness
-    population.sort_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
-    // println!("BEFORE selection - Population len {}", population.len());
+    // tournament selection
+    
+    let len = population.len();
+    let mut rng = rand::thread_rng();
 
-    // remove worst half of the population for slice
-    population.truncate(population.len() / 2);
-    // for i in 0..population.len(){
-    // println!("Fitness after {}", population[i].fitness);
-    // }
-    // println!("AFTER selection - Population len {}", population.len());
+    // iterate through the population
+    for _ in 0..len/2{
+        // select two random individuals
+        let mut idx_one = rng.gen_range(0..population.len());
+        let mut idx_two = rng.gen_range(0..population.len());
+        while idx_one == idx_two {
+            idx_one = rng.gen_range(0..population.len());
+        }
+
+        // choose the individuals with the highest fitness
+        if population[idx_one].fitness < population[idx_two].fitness {
+            population.remove(idx_one);
+        } else {
+            population.remove(idx_two);
+        }
+    }
 }
 
 fn mutation(state: &mut WsgState) {
     let mut rng = rand::thread_rng();
 
     if rng.gen_bool(MUTATION_RATE) {
-        // state.energy_consume = rng.gen_range(1.0..=2.0);
+        let switch = rng.gen_range(0..5);
+        match switch {
+            0 => state.gain_energy_sheep = rng.gen_range(1.0..=50.0),
+            1 => state.gain_energy_wolf = rng.gen_range(1.0..=50.0),
+            2 => state.sheep_repr = rng.gen_range(0.01..=0.2),
+            3 => state.wolf_repr = rng.gen_range(0.0..=0.2),
+            4 => state.full_grown = rng.gen_range(10..40),
+            _ => panic!("Invalid mutation switch")
+        }
     }
-    // println!("New energy {}", state.energy_consume);
 }
 
 fn crossover(population: &mut Vec<WsgState>) {
-    // iterates on population two by two
-    // for i in (0..population.len()/2).step_by(2){
-    //     let parent_one = &population[i];
-    //     let parent_two = &population[i+1];
+    // tournament selection
 
-    //     // println!("Parent one {}", parent_one.fitness);
-    //     // println!("Parent two {}", parent_two.fitness);
-    //     let energy = (parent_one.energy + parent_two.energy) / 2.;
-    //     population[i+1].energy_consume = energy;
-    // }
+    let len = population.len();
+    let mut rng = rand::thread_rng();
+
+    // iterate through the population
+    for _ in 0..len{
+        // select two random individuals
+        let mut idx_one = rng.gen_range(0..population.len());
+        let mut idx_two = rng.gen_range(0..population.len());
+        while idx_one == idx_two {
+            idx_one = rng.gen_range(0..population.len());
+        }
+
+        // combines random parameters of the parents
+        let parents = vec![idx_one, idx_two];
+
+        // to create a new individual
+        let mut new_individual = WsgState::new(
+            population[*(parents.choose(&mut rng).unwrap())].gain_energy_sheep,
+            population[*(parents.choose(&mut rng).unwrap())].gain_energy_wolf,
+            population[*(parents.choose(&mut rng).unwrap())].sheep_repr,
+            population[*(parents.choose(&mut rng).unwrap())].wolf_repr,
+            population[*(parents.choose(&mut rng).unwrap())].full_grown, 
+        );
+        
+        // add the new individual to the population
+        population.push(new_individual);
+    }
+    
+    println!("Population len after crossover {}", population.len());
 }
 
 fn fitness(state: &mut WsgState, schedule: Schedule) -> f32 {
-    // let agents = schedule.get_all_events();
-    // let mut sheeps: f32 = 0.;
-    // let mut wolves: f32 = 0.;
-    // let mut num_sheeps: f32 = 0.;
-    // let mut num_wolves: f32 = 0.;
 
-    // for n in agents {
-    //     if let Some(s) = n.downcast_ref::<Sheep>() {
-    //         sheeps += s.energy;
-    //         num_sheeps += 1.;
-    //     }
-    //     if let Some(w) = n.downcast_ref::<Wolf>() {
-    //         wolves += w.energy;
-    //         num_wolves += 1.;
-    //     }
-    // }
+    /*
+    D = desired population
 
-    // if sheeps == 0. || wolves == 0. {
-    //     state.fitness = 0.;
-    //     println!("Zero.");
-    //     return 0.;
-    // }
+    PERC(wolf|sheep) = 
+    if mean(agent) <= D(agent)
+        media(agent) / D(agent)
+    else
+        1 - ( (abs(D(agent) - mean(agent))) / MAX_AGENT - D(agent) )
 
-    //let fitness = 1. / ((wolves - sheeps).abs() - (INITIAL_NUM_WOLVES as f32 - INITIAL_NUM_SHEEPS as f32).abs()).abs();
-    //let fitness = 1. / ((wolves - sheeps).abs() / (INITIAL_NUM_WOLVES as f32 - INITIAL_NUM_SHEEPS as f32).abs());
-    // let fitness: f32 = (sheeps + wolves) * (1. / (1. + (num_sheeps - num_wolves).abs()));
+    AVG =
+    if wolf = 0 || sheep = 0 
+        0
+    else
+        (PERC(wolf) + PERC(sheep)) / 2
+    */
 
-    // println!("Fitness: {}", fitness);
+    let desired_sheeps = 1000.;
+    let desired_wolves = 200.;
+    let max_agent = 5000.;
 
-    // let fitness = (INITIAL_NUM_WOLVES as f32 - INITIAL_NUM_SHEEPS as f32).abs() / (wolves - sheeps).abs() ;
-    // state.fitness = fitness;
-    // fitness
-    1.
+    let agents = schedule.get_all_events();
+    let mut num_sheeps: f32 = 0.;
+    let mut num_wolves: f32 = 0.;
+
+    for n in agents {
+        if let Some(_s) = n.downcast_ref::<Sheep>() {
+            num_sheeps += 1.;
+        }
+        if let Some(_w) = n.downcast_ref::<Wolf>() {
+            num_wolves += 1.;
+        }
+    }
+
+    let mean_agent = (num_sheeps + num_wolves) / 2.;
+
+    let mut perc_sheeps = 0.;
+    if mean_agent <= desired_sheeps{
+        perc_sheeps = mean_agent / desired_sheeps;
+    } else {
+        perc_sheeps = 1. - ( ((desired_sheeps - mean_agent).abs()) / (max_agent - desired_sheeps));
+    }
+
+    let mut perc_wolves = 0.;
+    if mean_agent <= desired_wolves{
+        perc_wolves = mean_agent / desired_wolves;
+    } else {
+        perc_wolves = 1. - ( ((desired_wolves - mean_agent).abs()) / (max_agent - desired_wolves));
+    }
+
+    let mut average;
+    
+    if num_wolves == 0. || num_sheeps == 0.{
+        average = 0.;
+    } else {
+        average = (perc_sheeps + perc_sheeps) / 2.;
+    }
+
+    state.fitness = average;
+    average
 }
