01626c241564fdc042535cc12ec109fc29edcac2
ddevin96 <danieledevinco1996@gmail.com>
2021-12-09 12:53:01 +0100

    parallel completed
    Co-authored-by: Giuseppe <gdambrosio@unisa.it>
    Co-authored-by: Luca Postiglione <zioposty@users.noreply.github.com>

--- wsg_fitness_exploration/src/main.rs
+++ wsg_fitness_exploration/src/main.rs
@@ -10,8 +10,8 @@ pub const ENERGY_CONSUME: f32 = 1.0;
 
 pub const MUTATION_RATE: f64 = 0.05;
 pub const DESIRED_FITNESS: f32 = 0.925;
-pub const MAX_GENERATION: u32 = 15;
-pub const POPULATION: u32 = 100;
+pub const MAX_GENERATION: u32 = 10;
+pub const POPULATION: u32 = 25;
 
 // Mutable parameters in the fitness
 // pub const FULL_GROWN: u16 = 20;
@@ -25,14 +25,14 @@ pub const INITIAL_NUM_SHEEPS: u32 = (100. * 0.6) as u32;
 
 pub const WIDTH: i32 = 25;
 pub const HEIGHT: i32 = 25;
-pub const STEP: u64 = 100;
+pub const STEP: u64 = 500;
 
+use rand::distributions::weighted::WeightedIndex;
+use rand::seq::SliceRandom;
 use {
     rust_ab::engine::{schedule::Schedule, state::State},
     rust_ab::*,
 };
-use rand::seq::SliceRandom;
-use rand::distributions::weighted::WeightedIndex;
 
 fn main() {
     // macro used to execute model exploration using a genetic algorithm
@@ -46,7 +46,7 @@ fn main() {
         DESIRED_FITNESS,
         MAX_GENERATION,
         STEP,
-        ComputationMode::Sequential,
+        ComputationMode::Parallel,
         parameters{
             gain_energy_sheep: f32
             gain_energy_wolf: f32
@@ -61,7 +61,7 @@ fn main() {
 fn init_population() -> Vec<WsgState> {
     // create an array of WsgState
     let mut population = Vec::new();
-    
+
     // create n=POPULATION individuals
     for _ in 0..POPULATION {
         let mut rng = rand::thread_rng();
@@ -89,26 +89,24 @@ fn init_population() -> Vec<WsgState> {
 }
 
 fn selection(population: &mut Vec<WsgState>) {
-
     // weighted tournament selection
     let mut rng = rand::thread_rng();
     let len = population.len();
     // build an array containing the fintess values in order to be used for the
     // weighted selection
     let mut weight = Vec::new();
-    
+
     for i in 0..len {
         weight.push((population[i].fitness * 100.).floor() as u32);
     }
 
     // iterate through the population
-    for _ in 0..len/2{
-
+    for _ in 0..len / 2 {
         let dist = WeightedIndex::new(&weight).unwrap();
         let parent_idx_one = dist.sample(&mut rng);
         let parent_idx_two;
 
-        if parent_idx_one == 0{
+        if parent_idx_one == 0 {
             parent_idx_two = parent_idx_one + 1;
         } else {
             parent_idx_two = parent_idx_one - 1;
@@ -146,12 +144,11 @@ fn selection(population: &mut Vec<WsgState>) {
 }
 
 fn crossover(population: &mut Vec<WsgState>) {
-
     let len = population.len();
     let mut rng = rand::thread_rng();
 
     // iterate through the population
-    for _ in 0..len{
+    for _ in 0..len {
         // select two random individuals
         let mut idx_one = rng.gen_range(0..population.len());
         let idx_two = rng.gen_range(0..population.len());
@@ -168,9 +165,9 @@ fn crossover(population: &mut Vec<WsgState>) {
             population[*(parents.choose(&mut rng).unwrap())].gain_energy_wolf,
             population[*(parents.choose(&mut rng).unwrap())].sheep_repr,
             population[*(parents.choose(&mut rng).unwrap())].wolf_repr,
-            population[*(parents.choose(&mut rng).unwrap())].full_grown, 
+            population[*(parents.choose(&mut rng).unwrap())].full_grown,
         );
-        
+
         // add the new individual to the population
         population.push(new_individual);
     }
@@ -188,24 +185,23 @@ fn mutation(state: &mut WsgState) {
             2 => state.sheep_repr = rng.gen_range(0.01..=0.2),
             3 => state.wolf_repr = rng.gen_range(0.0..=0.2),
             4 => state.full_grown = rng.gen_range(10..40),
-            _ => panic!("Invalid mutation switch")
+            _ => panic!("Invalid mutation switch"),
         }
     }
 }
 
 fn fitness(state: &mut WsgState, schedule: Schedule) -> f32 {
-
     /*
     D = desired population
 
-    PERC(wolf|sheep) = 
+    PERC(wolf|sheep) =
     if mean(agent) <= D(agent)
         media(agent) / D(agent)
     else
         1 - ( (abs(D(agent) - mean(agent))) / MAX_AGENT - D(agent) )
 
     AVG =
-    if wolf = 0 || sheep = 0 
+    if wolf = 0 || sheep = 0
         0
     else
         (PERC(wolf) + PERC(sheep)) / 2
@@ -231,22 +227,22 @@ fn fitness(state: &mut WsgState, schedule: Schedule) -> f32 {
     let mean_agent = (num_sheeps + num_wolves) / 2.;
 
     let perc_sheeps;
-    if mean_agent <= desired_sheeps{
+    if mean_agent <= desired_sheeps {
         perc_sheeps = mean_agent / desired_sheeps;
     } else {
-        perc_sheeps = 1. - ( ((desired_sheeps - mean_agent).abs()) / (max_agent - desired_sheeps));
+        perc_sheeps = 1. - (((desired_sheeps - mean_agent).abs()) / (max_agent - desired_sheeps));
     }
 
     let perc_wolves;
-    if mean_agent <= desired_wolves{
+    if mean_agent <= desired_wolves {
         perc_wolves = mean_agent / desired_wolves;
     } else {
-        perc_wolves = 1. - ( ((desired_wolves - mean_agent).abs()) / (max_agent - desired_wolves));
+        perc_wolves = 1. - (((desired_wolves - mean_agent).abs()) / (max_agent - desired_wolves));
     }
 
     let average;
-    
-    if num_wolves == 0. || num_sheeps == 0.{
+
+    if num_wolves == 0. || num_sheeps == 0. {
         average = 0.;
     } else {
         average = (perc_sheeps + perc_wolves) / 2.;
