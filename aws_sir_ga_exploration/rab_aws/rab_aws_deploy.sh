469eeb6dec5a5ac96eb09bfb56613692981db820
giusdam <giuseppe.dambrosio14@gmail.com>
2022-01-24 16:01:08 +0100

    minor aws

---
+++ aws_sir_ga_exploration/rab_aws/rab_aws_deploy.sh
@@ -0,0 +1,53 @@
+
+#!/bin/bash
+
+echo "Generating the json files required for lambda creation..."
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sqs:*"
+            ],
+            "Resource": "*" 
+        },
+        {
+            "Effect":"Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}' > rab_aws/policy.json
+    
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole" 
+        }
+    ]
+}' > rab_aws/rolePolicy.json
+
+echo "Creation of IAM Role rab_role..."
+role_arn=$(aws iam create-role --role-name rab_role --assume-role-policy-document file://rab_aws/rolePolicy.json --query 'Role.Arn')
+echo "IAM Role rab_role created at ARN "${role_arn//\"}
+
+echo "Attacching policy to IAM Role..."	
+aws iam put-role-policy --role-name rab_role --policy-name rab_policy --policy-document file://rab_aws/policy.json
+
+echo "Function building..."
+cross build --release --features aws --bin function --target x86_64-unknown-linux-gnu
+echo "Zipping the target for the upload..."
+cp ./target/x86_64-unknown-linux-gnu/release/function ./bootstrap && zip rab_aws/rab_lambda.zip bootstrap && rm bootstrap 
+
+echo "Creation of the lambda function..."
+aws lambda create-function --function-name rab_lambda --handler main --zip-file fileb://rab_aws/rab_lambda.zip --runtime provided.al2 --role ${role_arn//\"} --timeout 900 --memory-size 10240 --environment Variables={RUST_BACKTRACE=1} --tracing-config Mode=Active 
