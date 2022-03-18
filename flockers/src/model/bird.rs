e972d3617dd07ac51429dc4da730daebaa140d00
ddevin96 <danieledevinco1996@gmail.com>
2022-03-18 11:36:11 +0100

    corect toml for build on github pages Co-authored-by: Luca Postiglione <zioposty@users.noreply.github.com> Co-authored-by: Giuseppe <gdambrosio@unisa.it>

--- flockers/src/model/bird.rs
+++ flockers/src/model/bird.rs
@@ -133,8 +133,8 @@ impl Agent for Bird {
 
         self.last_d = Real2D { x: dx, y: dy };
 
-        let loc_x = toroidal_transform(self.pos.x + dx, width);
-        let loc_y = toroidal_transform(self.pos.y + dy, height);
+        let loc_x = toroidal_transform(self.loc.x + dx, width);
+        let loc_y = toroidal_transform(self.loc.y + dy, height);
 
         self.loc = Real2D { x: loc_x, y: loc_y };
         drop(vec);
