7298c496b326fefa1935e3dfac5942858a079f6a
zioposty <l.postiglione4@studenti.unisa.it>
2022-05-06 16:00:58 +0200

    init fix

--- wolfsheepgrass/src/main.rs
+++ wolfsheepgrass/src/main.rs
@@ -12,18 +12,18 @@ pub const SHEEP_REPR: f64 = 0.2;
 pub const WOLF_REPR: f64 = 0.1;
 
 pub const MOMENTUM_PROBABILITY: f64 = 0.8;
-use krabmaga::*;
 
 // No visualization specific imports
 #[cfg(not(any(feature = "visualization", feature = "visualization_wasm")))]
 use {
+    krabmaga::*,
     krabmaga::engine::schedule::Schedule, krabmaga::engine::state::State, krabmaga::simulate,
     std::time::Duration,
 };
 
 #[cfg(not(any(feature = "visualization", feature = "visualization_wasm")))]
 fn main() {
-    let step = 50;
+    let step = 200;
 
     let dim: (i32, i32) = (25, 25);
     let initial_animals: (u32, u32) = ((100. * 0.6) as u32, (100. * 0.4) as u32);
