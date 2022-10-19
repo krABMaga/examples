e73520536144e42d1a338e11d6ce4171076019f2
Luca Postiglione <45485134+zioposty@users.noreply.github.com>
2022-10-19 13:22:15 +0200

    Makefile updated + changed GitHub action (#22)
    * wasm-bindgen version removed
    
    * wasm-bindgen version removed
    
    * wasm fixed for sugarscape
    
    * cargo fmt
    
    * Action fixed to work on merges

--- sugarscape/src/main.rs
+++ sugarscape/src/main.rs
@@ -1,26 +1,28 @@
 mod model;
 use crate::model::state::Environment;
-use cfg_if::cfg_if;
+use krabmaga::cfg_if::cfg_if;
 use {
     krabmaga::engine::schedule::Schedule, krabmaga::engine::state::State, krabmaga::*,
     std::time::Duration,
 };
 
-cfg_if! {
-    if #[cfg(any(feature = "parallel", feature = "visualization", feature = "visualization_wasm"))]{
-        mod visualization;
-        use crate::visualization::environment_vis::EnvironmentVis;
-        use crate::visualization::eater_vis::EaterVis;
-        use {
-            krabmaga::bevy::prelude::Color,
-            krabmaga::bevy::prelude::IntoSystem,
-            krabmaga::engine::fields::dense_object_grid_2d::DenseGrid2D,
-            krabmaga::engine::fields::dense_number_grid_2d::DenseNumberGrid2D,
-            krabmaga::visualization::fields::number_grid_2d::BatchRender,
-            krabmaga::visualization::visualization::Visualization,
-        };
-    }
-}
+#[cfg(any(feature = "visualization", feature = "visualization_wasm"))]
+mod visualization;
+
+#[cfg(any(feature = "visualization", feature = "visualization_wasm"))]
+use crate::visualization::environment_vis::EnvironmentVis;
+
+#[cfg(any(feature = "visualization", feature = "visualization_wasm"))]
+use crate::visualization::eater_vis::EaterVis;
+
+#[cfg(any(feature = "visualization", feature = "visualization_wasm"))]
+use {
+    krabmaga::bevy::prelude::Color, krabmaga::bevy::prelude::IntoSystem,
+    krabmaga::engine::fields::dense_number_grid_2d::DenseNumberGrid2D,
+    krabmaga::engine::fields::dense_object_grid_2d::DenseGrid2D,
+    krabmaga::visualization::fields::number_grid_2d::BatchRender,
+    krabmaga::visualization::visualization::Visualization,
+};
 
 pub const MAX_SUGAR: u32 = 3;
 
@@ -37,8 +39,6 @@ fn main() {
 
 #[cfg(any(feature = "visualization", feature = "visualization_wasm"))]
 fn main() {
-    let step = 10;
-
     let dim = (75, 75);
     let num_agents = 150;
 
