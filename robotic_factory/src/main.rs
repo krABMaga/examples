dc756d13b20a51ad3c26405a100f4de3fd68666b
LWagner <st148345@stud.uni-stuttgart.de>
2023-02-05 20:05:17 +0100

    Add main for non-visualized version

--- robotic_factory/src/main.rs
+++ robotic_factory/src/main.rs
@@ -1,14 +1,9 @@
-use krabmaga::engine::fields::field_2d::Location2D;
-use krabmaga::engine::location::Real2D;
-use krabmaga::engine::schedule::Schedule;
-use krabmaga::engine::state::State;
+extern crate core;
 
-use model::robot_factory::*;
-use model::stations::*;
+use model::robot_factory::RobotFactory;
 
 mod model;
 mod visualization;
-
 
 pub const ROBOT_COUNT: usize = 3;
 pub const JUST_IN_TIME_CHARGE: u32 = 50;
@@ -22,24 +17,17 @@ pub const ENERGY_COST_PER_STEP_WHILE_CARRYING: i32 = 2;
 pub const FACTORY_WIDTH: f32 = 100.0;
 pub const FACTORY_HEIGHT: f32 = 100.0;
 
+pub const STEP: u64 = 100;
+
+// No visualization specific imports
+#[cfg(not(any(feature = "visualization", feature = "visualization_wasm")))]
+use krabmaga::*;
+
+#[cfg(not(any(feature = "visualization", feature = "visualization_wasm")))]
 fn main() {
-    println!("Hello, world!");
-
-    let mut factory = RobotFactory::new();
-
-    let mut schedule = Schedule::new();
-
-    factory.init(&mut schedule);
-    factory.update(0);
-
-
-    let robot_room_location = factory.get_random_station_location_with_type(StationType::RobotRoom);
-
-    let neighbors = factory.robot_grid.get_neighbors_within_distance(robot_room_location.location, 2.0);
-
-    for mut neighbor_robot in neighbors {
-        neighbor_robot.charge(5, &factory);
-    }
+    let factory = RobotFactory::new();
+    simulate!(factory, STEP, 1);
+}
 
 
     factory.update(1);
